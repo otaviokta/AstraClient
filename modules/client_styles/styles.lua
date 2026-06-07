e857bce64cc235edbf4a0a7eb7440ae77cc0d595
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2026-06-06 22:47:45 -0300

    fix: sync styles.lua with mehah pattern, add pcall protection
    - Use importResources() generic loader (matching mehah)
    - Add ttf/otf font support
    - Wrap font loading in pcall to prevent single bad font from crashing all
    - Use full path matching via listDirectoryFiles(..., true, false, true)

--- modules/client_styles/styles.lua
+++ modules/client_styles/styles.lua
@@ -1,58 +1,52 @@
+local resourceLoaders = {
+    ["otui"] = g_ui.importStyle,
+    ["otfont"] = g_fonts.importFont,
+    ["ttf"] = g_fonts.importFont,
+    ["otf"] = g_fonts.importFont,
+    ["otps"] = g_particles.importParticle,
+}
+
 function init()
-  local files
-  local loaded_files = {}
-  local layout = g_resources:getLayout()
+    local device = g_platform.getDevice()
+    importResources("styles", "otui", device)
+    importResources("fonts", "otfont", device)
+    importResources("fonts", "ttf", device)
+    importResources("fonts", "otf", device)
+    importResources("particles", "otps", device)
 
-  local style_files = {}
-  if layout:len() > 0 then
-    loaded_files = {}
-    files = g_resources.listDirectoryFiles('/layouts/' .. layout .. '/styles')
-    for _,file in pairs(files) do
-      if g_resources.isFileType(file, 'otui') then
-        table.insert(style_files, file)
-        loaded_files[file] = true
-      end
-    end
-  end
-
-  files = g_resources.listDirectoryFiles('/data/styles')
-  for _,file in pairs(files) do
-    if g_resources.isFileType(file, 'otui') and not loaded_files[file] then
-        table.insert(style_files, file)
-    end
-  end
-
-  table.sort(style_files)
-  for _,file in pairs(style_files) do
-    if g_resources.isFileType(file, 'otui') then
-      g_ui.importStyle('/styles/' .. file)
-    end
-  end
-
-  if layout:len() > 0 then
-    files = g_resources.listDirectoryFiles('/layouts/' .. layout .. '/fonts')
-    loaded_files = {}
-    for _,file in pairs(files) do
-      if g_resources.isFileType(file, 'otfont') then
-        g_fonts.importFont('/layouts/' .. layout .. '/fonts/' .. file)
-        loaded_files[file] = true
-      end
-    end
-  end
-
-  files = g_resources.listDirectoryFiles('/data/fonts')
-  for _,file in pairs(files) do
-    if g_resources.isFileType(file, 'otfont') and not loaded_files[file] then
-      g_fonts.importFont('/data/fonts/' .. file)
-    end
-  end
-
-  g_mouse.loadCursors('/data/cursors/cursors')
-  if layout:len() > 0 and g_resources.directoryExists('/layouts/' .. layout .. '/cursors/cursors') then
-    g_mouse.loadCursors('/layouts/' .. layout .. '/cursors/cursors')
-  end
+    g_mouse.loadCursors('/data/cursors/cursors')
 end
 
 function terminate()
 end
 
+function importResources(dir, type, device)
+    local path = '/' .. dir .. '/'
+    local files = g_resources.listDirectoryFiles(path, true, false, true)
+    for _, file in pairs(files) do
+        if g_resources.isFileType(file, type) then
+            local success, err = pcall(resourceLoaders[type], file)
+            if not success then
+                g_logger.warning("Failed to load resource: " .. tostring(file) .. " (" .. tostring(err) .. ")")
+            end
+        end
+    end
+
+    if device then
+        local devicePath = g_platform.getDeviceShortName(device.type)
+        if devicePath ~= "" then
+            importResources(dir .. '/' .. devicePath, type)
+        end
+        local osPath = g_platform.getOsShortName(device.os)
+        if osPath ~= "" then
+            importResources(dir .. '/' .. osPath, type)
+        end
+    end
+    return files
+end
+
+function reloadParticles()
+    g_particles.terminate()
+    local device = g_platform.getDevice()
+    importResources("particles", "otps", device)
+end
